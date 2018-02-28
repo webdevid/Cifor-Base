035692006aacbdc5639592ab54c6dbf7f89b0fd3
Dodi Hidayatullah <dodihidayatullah@gmail.com>
2018-02-28 14:49:42 +0700

    commit

---
+++ cifor-base.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  CIFOR Base
+Plugin URI:   https://github.com/CIFORforests
+Description:  -
+Version:      1.0.0
+Author:       Cifor web team
+Author URI:   <a href="https://github.com/CIFORforests">https://github.com/CIFORforests</a>
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  cifor-base
+Domain Path:  /languages
+*/
